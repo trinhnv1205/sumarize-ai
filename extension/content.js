279d8c3aeafb5e1b248dc1629a112f19b5f3e677
trinhnv <blackwizard1205@gmail.com>
2024-10-12 18:48:34 +0700

    Refactor API endpoint in content.js and update extension version in manifest.json

--- extension/content.js
+++ extension/content.js
@@ -166,7 +166,7 @@ function fetchData() {
 
   document.getElementById("loading-spinner").style.display = "block";
 
-  fetch("https://ai-proxy-31b697729d07.herokuapp.com/api/generate_summary", {
+  fetch("[YOUR API]/api/generate_summary", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
